abe209f2dea907b6b74d2d6eaae8a1460290ffe2
ibnuyezid <tewdudai@gmail.com>
2024-04-16 11:59:26 +0300

    imp:contact&payment UI

--- lib/constants.dart
+++ lib/constants.dart
@@ -201,3 +201,85 @@ class PrimaryColors {
   static Color get yellow700 => const Color(0XFFFBC02D);
   static Color get yellow800 => const Color(0XFFF79E1B);
 }
+
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+// 29_Location_screen_confirm images
+  static String imgRectangle553 = '$imagePath/img_rectangle_553.png';
+
+  static String imgLinkedin = '$imagePath/img_linkedin.svg';
+
+  static String imgCall = '$imagePath/img_call.svg';
+
+  static String imgMessageSvgrepoCom = '$imagePath/img_message_svgrepo_com.svg';
+
+// 30_Message_screen images
+  static String imgEllipse42 = '$imagePath/img_ellipse_42.png';
+  static String hamburgermenu = '$imagePath/Hamburger Menu.png';
+
+  static String imgClose = '$imagePath/img_close.svg';
+  static String imgCamera = '$imagePath/Camera.svg';
+  static String imgHistory = '$imagePath/History.png';
+
+  static String imgUser = '$imagePath/img_user.svg';
+  static String imgComplain = '$imagePath/Complain.png';
+  static String imgReferral = '$imagePath/Referral.png';
+  static String imgsuccess = '$imagePath/success.png';
+  static String imgabout = '$imagePath/about_us.png';
+  static String imgemoji = '$imagePath/face_emoji.png';
+  static String imglogout = '$imagePath/Logout.png';
+  static String imgsetting = '$imagePath/Settings.png';
+  static String imgsupport = '$imagePath/support.png';
+
+  static String imgTelevisionIndigo100 =
+      '$imagePath/img_television_indigo_100.svg';
+
+// 33_payment images
+  static String imgPayment = '$imagePath/img_payment.svg';
+
+  static String imgThumbsup = '$imagePath/img_thumbsup.svg';
+
+// Cancel Ride images
+  static String imgCheckmarkOnprimarycontainer =
+      '$imagePath/img_checkmark_onprimarycontainer.svg';
+
+// Common images
+  static String imgArrowRight = '$imagePath/img_arrow_right.svg';
+
+  static String imgSignal = '$imagePath/img_signal.svg';
+
+  static String imgImage3 = '$imagePath/img_image_3.png';
+
+  static String imgTelevision = '$imagePath/img_television.svg';
+
+  static String imgArrowLeft = '$imagePath/img_arrow_left.svg';
+
+  static String imgUserYellow800 = '$imagePath/img_user_yellow_800.svg';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
+
+class BorderRadiusStyle {
+  // Circle borders
+  static BorderRadius get circleBorder18 => BorderRadius.circular(
+        18.h,
+      );
+// Custom borders
+  static BorderRadius get customBorderBL16 => BorderRadius.only(
+        topRight: Radius.circular(16.h),
+        bottomLeft: Radius.circular(16.h),
+        bottomRight: Radius.circular(16.h),
+      );
+  static BorderRadius get customBorderTL24 => BorderRadius.vertical(
+        top: Radius.circular(24.h),
+      );
+// Rounded borders
+  static BorderRadius get roundedBorder4 => BorderRadius.circular(
+        4.h,
+      );
+  static BorderRadius get roundedBorder8 => BorderRadius.circular(
+        8.h,
+      );
+}
---
+++ lib/screens/payment/widget/review_modalsheet.dart
@@ -0,0 +1,292 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:travelwave_mobile/constants.dart';
+import 'package:travelwave_mobile/widgets/custom_button.dart';
+import 'package:travelwave_mobile/widgets/custom_image_view.dart';
+
+class ReviewScreenBottomsheet extends StatefulWidget {
+  const ReviewScreenBottomsheet({super.key});
+
+  @override
+  State<ReviewScreenBottomsheet> createState() =>
+      _ReviewScreenBottomsheetState();
+}
+
+class _ReviewScreenBottomsheetState extends State<ReviewScreenBottomsheet> {
+  final List<String> tips = ["\$0", "\$2", "\$5", "\$10", "\$20"];
+
+  String _selected = "";
+  void showThanksDialog(context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10.h),
+            child: Container(
+              color: Theme.of(context).colorScheme.background,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                      icon: const Icon(Icons.close),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 23.h),
+                    child: Column(children: [
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 20.v),
+                        child: Image.asset(
+                          ImageConstant.imgsuccess,
+                          scale: 1.4,
+                        ),
+                      ),
+                      Text('Thank You',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleMedium
+                              ?.copyWith(fontWeight: FontWeight.w600)),
+                      SizedBox(height: 10.v),
+                      Text('Thank you for your valuable feedback and tip',
+                          textAlign: TextAlign.center,
+                          style:
+                              Theme.of(context).textTheme.bodyMedium?.copyWith(
+                                    fontWeight: FontWeight.w400,
+                                  )),
+                    ]),
+                  ),
+
+                  SizedBox(
+                    height: 16.v,
+                  ),
+
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 13.h, vertical: 10.v)
+                            .copyWith(top: 26.v),
+                    child: CustomElevatedButton(
+                        onPressed: () {
+                          // print("object");
+                          // reviewDialog(context);
+                        },
+                        // width: 189.h,
+                        center: true,
+                        text: "Back Home",
+                        buttonTextStyle: Theme.of(context)
+                            .textTheme
+                            .bodyMedium
+                            ?.copyWith(
+                                color:
+                                    Theme.of(context).colorScheme.background)),
+                  )
+                  // Align(
+                  //   alignment: Alignment.bottomCenter,
+                  //   child: RaisedButton(
+                  //     child: Text('Submit'),
+                  //     onPressed: () {
+                  //       // Perform submit action
+                  //     },
+                  //   ),
+                  // ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Column(mainAxisSize: MainAxisSize.min, children: [
+        ListView(shrinkWrap: true, reverse: true, children: [
+          Container(
+            width: double.maxFinite,
+            padding: EdgeInsets.symmetric(
+              horizontal: 7.h,
+              vertical: 9.v,
+            ),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.background,
+              borderRadius: BorderRadiusStyle.customBorderTL24,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CustomImageView(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  imagePath: ImageConstant.imgArrowRight,
+                  height: 24.adaptSize,
+                  width: 24.adaptSize,
+                  alignment: Alignment.centerRight,
+                ),
+                SizedBox(height: 33.v),
+                RatingBar.builder(
+                  ignoreGestures: false,
+                  initialRating: 5,
+                  minRating: 0,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemSize: 24.v,
+                  unratedColor: PrimaryColors.yellow700,
+                  itemCount: 5,
+                  updateOnDrag: true,
+                  itemBuilder: (context, _) {
+                    return Icon(
+                      Icons.star,
+                      color: PrimaryColors.yellow700,
+                    );
+                  },
+                  onRatingUpdate: (rating) {
+                    // print(rating);
+                  },
+                ),
+                SizedBox(height: 21.v),
+                Text(
+                  "Excellent",
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        fontSize: 20.fSize,
+                      ),
+                ),
+                SizedBox(height: 7.v),
+                Text(
+                  "You rated Sergio Ramasis 4 star",
+                  style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.secondaryContainer,
+                      ),
+                ),
+                SizedBox(height: 22.v),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Write your text',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                  maxLines: 4,
+                ),
+                // Padding(
+                //   padding: EdgeInsets.only(
+                //     left: 8.h,
+                //     right: 9.h,
+                //   ),
+                //   child: CustomTextFormField(
+                //     // controller: inputoneController,
+                //     hintText: "lbl_write_your_text".tr,
+                //     prefix: Container(
+                //       margin: EdgeInsets.fromLTRB(17.h, 30.v, -52.h, 14.v),
+                //       child: CustomImageView(
+                //         imagePath: ImageConstant.imgUserYellow800,
+                //         height: 35.v,
+                //         width: 45.h,
+                //       ),
+                //     ),
+                //     prefixConstraints: BoxConstraints(
+                //       maxHeight: 118.v,
+                //     ),
+                //     maxLines: 5,
+                //     contentPadding: EdgeInsets.only(
+                //       top: 18.v,
+                //       right: 10.h,
+                //       bottom: 18.v,
+                //     ),
+                //   ),
+                // ),
+                SizedBox(height: 24.v),
+                Text(
+                  "Give some tips to Sergio Ramasis",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                SizedBox(height: 21.v),
+                Wrap(
+                    direction: Axis.horizontal,
+                    spacing: 15,
+                    children: tips
+                        .map(
+                          (tip) => GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _selected = tip;
+                              });
+                            },
+                            child: Container(
+                              width: 60.h,
+                              height: 60.v,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: _selected == tip
+                                      ? PrimaryColors.amberA400
+                                      : Colors.grey,
+                                  width: 1.0,
+                                ),
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              child: Center(
+                                child: Text(tip,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleMedium
+                                        ?.copyWith(
+                                            color: tip == _selected
+                                                ? PrimaryColors.amberA400
+                                                : null)),
+                              ),
+                            ),
+                          ),
+                        )
+                        .toList()),
+                // Padding(
+                //     padding: EdgeInsets.only(
+                //       left: 8.h,
+                //       right: 9.h,
+                //     ),
+                //     child: CustomPinCodeTextField(
+                //       context: context,
+                //       // controller: otpController,
+                //       onChanged: (value) {
+                //         // otpController?.text = value;
+                //       },
+                //     )),
+                SizedBox(height: 11.v),
+                // Text(
+                //   "msg_enter_other_amount".tr,
+                //   style: CustomTextStyles.labelLargeAmberA400,
+                // ),
+                SizedBox(height: 20.v),
+                CustomElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      showThanksDialog(context);
+                    },
+                    text: "Submit",
+                    center: true,
+                    margin: EdgeInsets.only(
+                      left: 8.h,
+                      right: 9.h,
+                    ),
+                    buttonTextStyle:
+                        Theme.of(context).textTheme.titleMedium!.copyWith(
+                              color: Theme.of(context).colorScheme.background,
+                            )),
+                SizedBox(height: 29.v)
+              ],
+            ),
+          ),
+        ]),
+      ]),
+    );
+  }
+}
