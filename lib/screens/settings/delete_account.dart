724cb72b8a691e20a6bc826afc4c40ba17365348
ibnuyezid <tewdudai@gmail.com>
2024-04-16 11:06:53 +0300

    imp:setting ui

---
+++ lib/screens/settings/delete_account.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:travelwave_mobile/constants.dart';
+import 'package:travelwave_mobile/widgets/custom_button.dart';
+
+class DeleteAccountScreen extends StatelessWidget {
+  const DeleteAccountScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Delete Account",
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        leadingWidth: 130.h,
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Row(
+            children: [
+              SizedBox(width: 16.h),
+              const Icon(Icons.arrow_back_ios),
+              Expanded(
+                child: Text(
+                  "Back",
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12.h, vertical: 12.v),
+            child: Text(
+              textAlign: TextAlign.start,
+              "Are you sure you want to delete your account? Please read how account deletion will affect.\nDeleting your account removes personal information our database. Tour email becomes permanently reserved and same email cannot be re-use to register a new account.",
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                  height: 1.6,
+                  color: PrimaryColors.gray500,
+                  fontWeight: FontWeight.w400),
+            ),
+          ),
+          CustomElevatedButton(
+            onPressed: () {},
+            text: "Delete",
+            center: true,
+            buttonStyle: ButtonStyle(
+              side: MaterialStateProperty.all<BorderSide>(BorderSide.none),
+              backgroundColor:
+                  MaterialStateProperty.all<Color>(const Color(0xffF44336)),
+            ),
+            margin: EdgeInsets.only(
+              left: 15.h,
+              right: 16.h,
+              bottom: 42.v,
+            ),
+            buttonTextStyle: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.background,
+                ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/settings/index.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:travelwave_mobile/constants.dart';
+import 'package:travelwave_mobile/screens/settings/change_password.dart';
+import 'package:travelwave_mobile/screens/settings/delete_account.dart';
+import 'package:travelwave_mobile/screens/settings/privacy_policy.dart';
+import 'package:travelwave_mobile/screens/settings/widget/setting_option_list.dart';
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Settings",
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        leadingWidth: 150.h,
+        leading: Row(
+          children: [
+            SizedBox(width: 16.h),
+            const Icon(Icons.arrow_back_ios),
+            Expanded(
+              child: Text(
+                "Back",
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+            )
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          SettingListTile(
+            option: "Change Password",
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return const ChangePassword();
+                },
+              ));
+            },
+          ),
+          SettingListTile(
+            option: "Change Language",
+            onPressed: () {},
+          ),
+          SettingListTile(
+            option: "Privacy Policy",
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return const PrivacyPolicyScreen();
+                },
+              ));
+            },
+          ),
+          SettingListTile(
+            option: "Delete Account",
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return const DeleteAccountScreen();
+                },
+              ));
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
